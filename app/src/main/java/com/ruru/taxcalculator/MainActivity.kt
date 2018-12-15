d878b0546cc381e36e5033b7dc190079b1e9ec13
binplumage <balaja2179@gmail.com>
2018-12-15 20:55:24 +0800

    feat: add function to show number and clear number.

--- app/src/main/java/com/ruru/taxcalculator/MainActivity.kt
+++ app/src/main/java/com/ruru/taxcalculator/MainActivity.kt
@@ -2,11 +2,53 @@ package com.ruru.taxcalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var computeButton:Button
+    internal lateinit var clearButton:Button
+    internal lateinit var tax:TextView
+    internal lateinit var taxExcluedText:TextView
+    internal lateinit var displayNumber:TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        computeButton = findViewById<Button>(R.id.compute)
+        clearButton = findViewById<Button>(R.id.clearButton)
+        tax = findViewById<TextView>(R.id.taxValue)
+        taxExcluedText = findViewById<TextView>(R.id.taxExcludedValue)
+        displayNumber = findViewById<TextView>(R.id.inputValue)
+    }
+
+
+    fun showNum (view: View) {
+        var numberButton = findViewById<Button>(view.id).text.toString()
+        var nowTotalNumber = displayNumber.text.toString()
+        if (nowTotalNumber == '0'.toString()) {
+            displayNumber.text = numberButton
+        }
+        else {
+            displayNumber.text = nowTotalNumber + numberButton
+        }
+    }
+
+    fun clearNumber(view: View){
+        displayNumber.text = "0"
+        tax.text = "0"
+        taxExcluedText.text = "0"
     }
 }
